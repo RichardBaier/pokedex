1bedf4721627e03a51ff0c69b69df9f76e3e6277
Aaron Morgan <aaron.morgan.7@outlook.com>
2022-10-24 21:14:17 -0600

    Added pokemon API

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,49 @@ $('#searchBtn').click(() => {
 //     const title = data.title;
 //     const thumbnail = 
 // }
+
+
+
+
+
+        //replace this declaration with the text box value!!
+//pokemonName = document.querySelector("text")
+var pokemonName = "charmander";
+
+var imageUrl = '';
+var imageIcon = document.createElement('img');
+var pokemonType = '';
+var pokemonId = 0;
+var height = 0;
+var weight = 0;
+
+                        //.value when query selector is in place
+var APIURL = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+
+
+function getPokemon(){
+    fetch(APIURL)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(pokemon){
+        height = pokemon.height;
+        weight = pokemon.weight;
+        pokemonId = pokemon.id;
+        imageUrl = pokemon.sprites.other.dream_world.front_default;
+
+        //image icon displays a picture of the pokemon
+        imageIcon.setAttribute('src', imageUrl)
+        
+        pokemonType = pokemon.types[0].type.name
+        console.log('height :>> ', height);
+        console.log('weight :>> ', weight);
+        console.log('pokemonType :>> ', pokemonType);
+        console.log('pokemonId :>> ', pokemonId);
+        console.log('image :>> ', imageIcon);
+    })
+}
+
+//invoke the function to run
+
+getPokemon();
